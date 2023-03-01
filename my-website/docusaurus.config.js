8b411944554fb337f6428da9a3cef219dfb0592e
amazingLychee <834994869@qq.com>
2023-03-01 17:49:12 +0800

    change user

--- my-website/docusaurus.config.js
+++ my-website/docusaurus.config.js
@@ -11,15 +11,16 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://amazingLychee.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'amazingLychee', // Usually your GitHub org/user name.
+  projectName: 'amazingLychee.github.io', // Usually your repo name.
+  deploymentBranch: 'gh-pages',
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
--- my-website/src/pages/index.tsx
+++ my-website/src/pages/index.tsx
@@ -18,7 +18,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Docusaurus Tutorial - 6min ⏱️
+            Docusaurus Tutorial - 7min ⏱️
           </Link>
         </div>
       </div>
